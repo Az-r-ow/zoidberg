5ffdc96b7dc5666791f0987ab2ce5bbff7d59189
Az-r-ow <antoine.azar123@gmail.com>
2024-04-11 23:19:14 +0200

    added upload dataset script

---
+++ upload_dataset_to_hf.py
@@ -0,0 +1,8 @@
+"""
+  In this file we'll be processing and uploading the chest_xray dataset to Hugging Face
+"""
+from datasets import load_dataset
+
+dataset = load_dataset("imagefolder", data_dir="./datasets/chest_Xray")
+
+dataset.push_to_hub("Az-r-ow/chest_xray")
